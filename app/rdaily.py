d11a352f5a2f42b8cc59cbc86fa07f59843bfa2c
Widoyo <widoyo@gmail.com>
2024-04-20 16:01:47 +0700

    Tambah Badge PCH/PDA

--- app/rdaily.py
+++ app/rdaily.py
@@ -1,6 +1,6 @@
 from flask import Blueprint, render_template, request
 import datetime
-from app.models import RDaily
+from app.models import RDaily, OPos
 
 bp = Blueprint('rdaily', __name__, url_prefix='/rdaily')
 
@@ -17,8 +17,9 @@ def show(pos_name):
         sampling_ = None
     if sampling.date() >= datetime.date.today():
         sampling_ = None
-        
+    pos = OPos.get(OPos.nama==pos_name)    
     this_day = RDaily.select().where(RDaily.nama==pos_name, RDaily.sampling==sampling).first()
+    this_day.tipe = pos.tipe
     return render_template('rdaily/show.html', sampling=sampling, this_day=this_day)
 
 @bp.route('/')
@@ -34,14 +35,19 @@ def index():
         sampling_ = None
     if sampling.date() >= datetime.date.today():
         sampling_ = None
+    poses = dict([(p.nama, p) for p in OPos.select()])
     sa_dailies = RDaily.select().where(RDaily.source=='SA', 
                                        RDaily.sampling.year==sampling.year, 
                                        RDaily.sampling.month==sampling.month, 
                                        RDaily.sampling.day==sampling.day)
+    for s in sa_dailies:
+        s.tipe = poses[s.nama].tipe
     sb_dailies = RDaily.select().where(RDaily.source=='SB', 
                                        RDaily.sampling.year==sampling.year, 
                                        RDaily.sampling.month==sampling.month, 
                                        RDaily.sampling.day==sampling.day)
+    for s in sb_dailies:
+        s.tipe = poses[s.nama].tipe
     return render_template('rdaily/index.html', 
                            sa_dailies=sa_dailies,
                            sb_dailies=sb_dailies,
