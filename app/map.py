7e63fa0eacd2f22f10645ae1d654a0610a67d80b
Widoyo <widoyo@gmail.com>
2024-05-28 13:17:24 +0700

    Tampilkan Pos Klimat dan Perbaikan menu pada Map

--- app/map.py
+++ app/map.py
@@ -7,10 +7,13 @@ bp = Blueprint('map', __name__, url_prefix='/map')
 
 @bp.route('/')
 def index():
-    pchs = Pos.select().where(Pos.tipe=='1')
-    pdas = Pos.select().where(Pos.tipe=='2')
+    poses = Pos.select().order_by(Pos.tipe, Pos.nama)
+    pchs = [p for p in poses if p.tipe == '1']
+    pdas = [p for p in poses if p.tipe=='2']
+    pklimats = [p for p in poses if p.tipe=='3']
     ctx = {
         'pos_ch': pchs,
-        'pos_da': pdas
+        'pos_da': pdas,
+        'pos_klimats': pklimats
     }
     return render_template('map/index.html', ctx=ctx)
