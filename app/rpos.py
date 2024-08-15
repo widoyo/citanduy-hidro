6fef05a22f20c30fc15e0602d4c30c8f88515591
Widoyo <widoyo@gmail.com>
2024-08-15 17:27:12 +0700

    Bug pada RPOS, lupa belum update py file

--- app/rpos.py
+++ app/rpos.py
@@ -7,5 +7,20 @@ bp = Blueprint('rpos', __name__, url_prefix='/rpos')
 @bp.route('/')
 def index():
     oposes = OPos.select().order_by(OPos.latest_sampling.desc())
-    return render_template('rpos/index.html', 
-                           poses=oposes)
+    
+    ctx = {
+        'poses': oposes,
+        'sa': {
+            'all': [p for p in oposes if p.source == 'SA'],
+            'pch': [p for p in oposes if p.source == 'SA' and p.tipe=='PCH'],
+            'pda': [p for p in oposes if p.source == 'SA' and p.tipe=='PDA'],
+            'aktif': [p for p in oposes if p.source == 'SA' and p.aktif==True],
+        },
+        'sb': {
+            'all': [p for p in oposes if p.source == 'SB'],
+            'pch': [p for p in oposes if p.source == 'SB' and p.tipe=='Rain Fall'],
+            'pda': [p for p in oposes if p.source == 'SB' and p.tipe=='PDA'],
+            'aktif': [p for p in oposes if p.source == 'SB' and p.aktif==True],         
+        }
+    }
+    return render_template('rpos/index.html', ctx=ctx)
